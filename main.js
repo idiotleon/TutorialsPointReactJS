79243c72572699a46ba12c3c4a4d25e09833e507
Yang Lu <yang.lu3@huawei.com>
2018-01-05 13:59:52 -0800

    demo example: ReactJS - Redux
    The store is a place that holds the app\'s state.

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
+// The store is a place that holds the app's state.
+// It is very easy to create a store once you have reducers
+import React from 'react'
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './App.jsx'
+import toDoApp from './reducers/reducers'
+
+let store = createStore(toDoApp)
+let rootElement = document.getElementById('app')
+
+render(
+    <Provider store = {store}>
+        <App />
+    </Provider>,
+
+    rootElement
+)

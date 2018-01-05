adbccb5694b45fd38d4415fd7dd1a0ebc7ec9544
Yang Lu <yang.lu3@huawei.com>
2018-01-05 12:11:14 -0800

    demo example: ReactJS Router
    Create Components

---
+++ app/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Login extends Componet{
+    render(){
+        return(
+            <div>
+                <h2>Login</h2>
+            </div>
+        ); 
+    }
+}
+
+export default Login;

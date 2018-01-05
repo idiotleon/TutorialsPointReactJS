8a26874fafe1a3d61bfe09c343199f5a99ea2edf
Yang Lu <yang.lu3@huawei.com>
2018-01-05 12:30:40 -0800

    fix: typo of Component

--- app/Login.js
+++ app/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class Login extends Componet{
+class Login extends Component{
     render(){
         return(
             <div>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: './app/main.js',
+    entry: './app/Main.js',
     output:{
         filename: 'bundle.js'
     },

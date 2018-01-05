fd06c6948baf61c7d79e27b1cfe2fe33faccf13b
Yang Lu <leon@Yangs-MBP.home>
2018-01-04 20:34:23 -0800

    demo example: Nested Element, Attributes, JavaScript Expressions of ReactJS

--- App.jsx
+++ App.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 class App extends React.Component{
     render(){
+        var i = 1;
+        
         return (
             <div>
-                Hello World!!
+                {/* Nested Elements */}
+                <h1>Header</h1>
+                <h2>Content</h2>
+
+                {/* custom attribute */}
+                <p data-myattribute="somevalue">This is the content!!</p>
+                
+                {/* JavaScript expressions */}
+                <h1>{1+1}</h1>
+
+                {/* One cannot use if else statements inside JSX, instead we can use conditional(ternary) expressions */}
+                <h1>{i == 1 ? 'True!':'False'}</h1>
             </div>
         );
     }
